7ab0a66a7f76605096fa68a92c323f57d344eb93
shanmathibaskaran <shanmathibaskara1996@gmail.com>
2022-05-19 17:54:53 +0530

    new code

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,22 @@
+package org.add;
+
+public class GreensTech {
+	
+	private void greensOmr() {
+		System.out.println("selenium");
+		
+	}
+	private void greensAdayar() {
+		System.out.println("java");
+		// TODO Auto-generated method stub
+
+		
+	}
+	
+	public static void main(String[] args) {
+		GreensTech g=new GreensTech();
+		g.greensOmr();
+		g.greensAdayar();
+	}
+
+}
